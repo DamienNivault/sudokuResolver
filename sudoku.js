04edb896f3f79a9ea1b4a55344d85182c0bf8773
Dams <damien.nivault@supinternet.fr>
2019-03-07 22:53:43 +0100

    :sparkles: Added v2 IA

--- sudoku.js
+++ sudoku.js
@@ -1,15 +1,17 @@
-const grid = [
-  [0, 0, 8, 4, 0, 3, 5, 0, 6],
-  [0, 0, 3, 1, 0, 2, 0, 0, 4],
-  [0, 4, 5, 7, 0, 0, 0, 9, 0],
-  [6, 9, 0, 0, 0, 5, 0, 0, 7],
-  [0, 8, 0, 0, 0, 0, 0, 5, 0],
-  [4, 0, 0, 3, 0, 0, 0, 1, 8],
-  [0, 7, 0, 0, 0, 6, 2, 4, 0],
-  [1, 0, 0, 5, 0, 7, 8, 0, 0],
-  [8, 0, 6, 9, 0, 1, 3, 0, 0]
+"use strict";
+
+let grid = [
+  [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 2, 0, 8, 5, 0, 0, 4, 1],
+  [8, 6, 4, 0, 2, 1, 9, 0, 0],
+  [0, 9, 8, 5, 0, 0, 3, 0, 2],
+  [0, 0, 3, 9, 0, 0, 0, 7, 0],
+  [0, 1, 2, 0, 0, 7, 0, 9, 0],
+  [5, 0, 0, 0, 0, 6, 0, 2, 9],
+  [3, 4, 0, 2, 0, 0, 0, 6, 5],
+  [0, 0, 0, 1, 0, 0, 4, 0, 0]
 ];
-const grid2 = [
+let grid2 = [
   [5, 3, 0, 0, 7, 0, 0, 0, 0],
   [6, 0, 0, 1, 9, 5, 0, 0, 0],
   [0, 9, 8, 0, 0, 0, 0, 6, 0],
@@ -21,7 +23,7 @@ const grid2 = [
   [0, 0, 0, 0, 8, 0, 0, 7, 9]
 ];
 
-const grid3 = [
+let grid3 = [
   [4, 0, 2, 0, 1, 7, 0, 3, 0],
   [3, 0, 0, 0, 0, 0, 0, 0, 1],
   [0, 0, 0, 0, 9, 0, 6, 0, 0],
@@ -33,7 +35,7 @@ const grid3 = [
   [0, 4, 0, 1, 8, 0, 9, 0, 6]
 ];
 
-const grid4 = [
+let grid4 = [
   [0, 0, 0, 0, 0, 0, 0, 0, 0],
   [0, 0, 0, 0, 0, 3, 0, 8, 5],
   [0, 0, 1, 0, 2, 0, 0, 0, 0],
@@ -45,6 +47,28 @@ const grid4 = [
   [0, 0, 0, 0, 4, 0, 0, 0, 9]
 ];
 
+let grid5 = [
+  [1, 0, 0, 0, 0, 7, 0, 9, 0],
+  [0, 3, 0, 0, 2, 0, 0, 0, 8],
+  [0, 0, 9, 6, 0, 0, 5, 0, 0],
+  [0, 0, 5, 3, 0, 0, 9, 0, 0],
+  [0, 1, 0, 0, 8, 0, 0, 0, 2],
+  [6, 0, 0, 0, 0, 4, 0, 0, 0],
+  [3, 0, 0, 0, 0, 0, 0, 1, 0],
+  [0, 4, 0, 0, 0, 0, 0, 0, 7],
+  [0, 0, 7, 0, 0, 0, 3, 0, 0]
+];
+let grid6 = [
+  [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 3, 0, 8, 5],
+  [0, 0, 1, 0, 2, 0, 0, 0, 0],
+  [0, 0, 0, 5, 0, 7, 0, 0, 0],
+  [0, 0, 4, 0, 0, 0, 1, 0, 0],
+  [0, 9, 0, 0, 0, 0, 0, 0, 0],
+  [5, 0, 0, 0, 0, 0, 0, 7, 3],
+  [0, 0, 2, 0, 1, 0, 0, 0, 0],
+  [0, 0, 0, 0, 4, 0, 0, 0, 9]
+];
 //Pretty print function
 function prettyPrint(grid) {
   console.log("*-------*-------*-------*");
@@ -87,8 +111,19 @@ function findEmptyCell(grid, row, col) {
       }
     }
   }
-  //Res is pass to solveSudoku as the coordinates of the empty cell
+  //Res is passed to solveSudoku as the coordinates of the empty cell
   return res;
+}
+const emptyCells = [];
+function getListOfEmptyCell(grid) {
+  for (let i = 0; i < grid.length; i++) {
+    for (let j = 0; j < grid[i].length; j++) {
+      if (grid[i][j] === 0) {
+        let pos = { x: i, y: j };
+        emptyCells.push(pos);
+      }
+    }
+  }
 }
 
 function noConflicts(grid, row, col, num) {
@@ -101,7 +136,7 @@ function noConflicts(grid, row, col, num) {
 }
 // Function who check if there is two same number in a row
 function isRowOk(grid, row, num) {
-  for (var col = 0; col < 9; col++) {
+  for (let col = 0; col < 9; col++) {
     if (grid[row][col] == num) return false;
   }
 
@@ -109,7 +144,7 @@ function isRowOk(grid, row, num) {
 }
 // Function who check if there is two same number in a column
 function isColOk(grid, col, num) {
-  for (var row = 0; row < 9; row++) {
+  for (let row = 0; row < 9; row++) {
     if (grid[row][col] == num) return false;
   }
 
@@ -122,49 +157,50 @@ function isSquareOk(grid, row, col, num) {
   row = Math.floor(row / 3) * 3;
   col = Math.floor(col / 3) * 3;
 
-  for (var r = 0; r < 3; r++)
-    for (var c = 0; c < 3; c++) {
+  for (let i = 0; i < 3; i++) {
+    for (let j = 0; j < 3; j++) {
       //If iterate to check all the number of the square
-      if (grid[row + r][col + c] == num) return false;
+      if (grid[row + i][col + j] == num) return false;
     }
+  }
 
   return true;
 }
 
-function solveSudoku(grid, row, col) {
-  //At each iteration apply findEmptyCell
-  var cell = findEmptyCell(grid, row, col);
+function solveSudoku(grid, emptyCells, index) {
   //Apply coordinates of the empty cell (or not)
-  row = cell[0];
-  col = cell[1];
-  // Can apply row or column, its the same
-  if (row == -1) {
-    // If the grid is full, cell [-1 -1] is return and we can print the grid and return true
+  if (index == emptyCells.length) {
     prettyPrint(grid);
     return true;
   }
-
-  for (var num = 1; num <= 9; num++) {
+  let row = emptyCells[index].x;
+  let col = emptyCells[index].y;
+  for (let num = 1; num <= 9; num++) {
     //Verify all the conditions (col,row and square)
     if (noConflicts(grid, row, col, num)) {
       //Apply the number
+
       grid[row][col] = num;
       //process.stdout.write("\x1b[32m" + String(grid[row][col]) + "\x1b[0m");
 
       //Apply recursion
-      if (solveSudoku(grid, row, col)) {
+      if (solveSudoku(grid, emptyCells, index + 1)) {
         return true;
       } else {
-        //Binding 0 to the cell
         grid[row][col] = 0;
       }
+
+      //Binding 0 to the cell
     }
   }
   // Backtracking
   return false;
 }
 
+getListOfEmptyCell(grid3);
+
 prettyPrint(grid3);
 console.time("timer sudoku");
-solveSudoku(grid3, 0, 0);
+solveSudoku(grid3, emptyCells, 1);
+//solveSudoku(grid, emptyCells, 1);
 console.timeEnd("timer sudoku");
